4628b838de055636c36018f1184905561c442b09
laisut <laimonas.sutkus@gmail.com>
2017-05-07 18:57:33 +0300

    UI improvements. Add Convolutional neural network

--- DigitRecognition/graphpainter.py
+++ DigitRecognition/graphpainter.py
@@ -2,42 +2,61 @@ import tkinter
 from board import Board
 import numpy as np
 
+# number of pixels in the cell (height = width)
+cell_size = 15
+
+# number of cells in the grid (height = width)
+grid_size = 28
+
+# add borders to the drawn window
+border = 10
+
+# window size
+window_size = grid_size * cell_size
+
+# leave border for functional buttons (like erase, exit)
+buttons_border = 50
+
+# determine where the bottom left cell begins
+northwest_point = [border, border]
+
+# choose between painting mode and erasing mode
+erasing_mode = False
+
 
 def paint_number(function_ref):
-    # number of pixels in the cell (height = width)
-    cell_size = 15
-
-    # number of cells in the grid (height = width)
-    grid_size = 28
-
-    # add borders to the drawn window
-    border = 10
-
-    # window size
-    window_size = grid_size * cell_size
-
-    # leave border for functional buttons (like erase, exit)
-    buttons_border = 50
-
-    # determine where the bottom left cell begins
-    northwest_point = [border, border]
-
     def on_click(event):
+        global erasing_mode
         coordinates_x = (event.x - northwest_point[0]) / cell_size
         coordinates_y = (event.y - northwest_point[1]) / cell_size
         print('{:.2f} : {:.2f}'.format(coordinates_x, coordinates_y))
-        board.add_point(coordinates_x, coordinates_y)
+        if erasing_mode is True:
+            board.remove_point(coordinates_x, coordinates_y)
+        else:
+            board.add_point(coordinates_x, coordinates_y)
         function_ref(np.asarray(board.get_image()))
 
     def on_clicked_draw(event):
+        global erasing_mode
         coordinates_x = (event.x - northwest_point[0]) / cell_size
         coordinates_y = (event.y - northwest_point[1]) / cell_size
         print('{:.2f} : {:.2f}'.format(coordinates_x, coordinates_y))
-        board.add_point(coordinates_x, coordinates_y)
+        if erasing_mode is True:
+            board.remove_point(coordinates_x, coordinates_y)
+        else:
+            board.add_point(coordinates_x, coordinates_y)
         function_ref(np.asarray(board.get_image()))
 
     def clear_call_back():
         board.clear()
+
+    def erase_call_back():
+        global erasing_mode
+        erasing_mode = True
+
+    def draw_call_back():
+        global erasing_mode
+        erasing_mode = False
 
     root = tkinter.Tk()
     root.geometry('{}x{}'.format(window_size + border * 2, window_size + border * 2 + buttons_border))
@@ -55,7 +74,13 @@ def paint_number(function_ref):
     button_canvas.config(width=window_size + border * 2, height=buttons_border)
 
     button_clear = tkinter.Button(button_canvas, text="Clear", command=clear_call_back)
-    button_clear.grid(row=0, column=0, columnspan=2, sticky='w')
+    button_clear.grid(row=0, column=0, columnspan=1, sticky='w')
+
+    button_paint = tkinter.Button(button_canvas, text="Draw", command=draw_call_back)
+    button_paint.grid(row=0, column=1, columnspan=1, sticky='w')
+
+    button_erase = tkinter.Button(button_canvas, text="Erase", command=erase_call_back)
+    button_erase.grid(row=0, column=2, columnspan=1, sticky='w')
 
     board = Board(grid_size, cell_size, canvas)
     board.draw_grid()
--- DigitRecognition/mainUI.py
+++ DigitRecognition/mainUI.py
@@ -2,40 +2,55 @@ from tkinter import *
 from tkinter import messagebox
 from mode import *
 import neuralnetwork
+import convolutionalneuralnetwork
 import numpy as np
 from graphpainter import paint_number
 import threading
 import tensorflow as tf
 
+text_box_size = 100
+window_width = 300
+window_height = 700
 
 root = Tk()
-root.geometry('{}x{}'.format(500, 500))
+root.geometry('{}x{}'.format(window_width, window_height))
 root.resizable(width=False, height=False)
 
 tensorflow_graph = None
 current_mode = Mode.NONE
+
 neural_model = None
+convolutional_model = None
+
+nn_stats = ["", "", "", "", ""]
+cnn_stats = ["", "", "", "", ""]
 
 
-def hello_call_back():
+def selection_call_back():
     global current_mode
     selection = var.get()
     if selection == 1:
         current_mode = Mode.NN
     elif selection == 2:
-        current_mode == Mode.CNN
+        current_mode = Mode.CNN
     else:
         current_mode = Mode.BOTH
-    print(selection)
 
 
 def train_call_back():
     global neural_model
+    global convolutional_model
     global tensorflow_graph
+    global nn_stats
+    global cnn_stats
     if current_mode == Mode.NN:
-        neural_model = neuralnetwork.train_model()
+        neural_model, nn_stats = neuralnetwork.train_model()
+        nn_info.delete("1.0", END)
+        nn_info.insert(INSERT, "Simple neural network:\n" + ''.join(str(x) for x in nn_stats))
     elif current_mode == Mode.CNN:
-        pass
+        convolutional_model, cnn_stats = convolutionalneuralnetwork.train_model()
+        cnn_info.delete("1.0", END)
+        cnn_info.insert(INSERT, "Convolutional neural network:\n" + ''.join(str(x) for x in cnn_stats))
     elif current_mode == Mode.BOTH:
         pass
     else:
@@ -86,28 +101,55 @@ def load_call_back():
 
 var = IntVar()
 
-chooseNN = Radiobutton(root, text="Use simple neural network", variable=var, value=1, command=hello_call_back)
-chooseNN.grid(row=0, column=0, columnspan=2, sticky='w')
+chooseNN = Radiobutton(root, text="Use simple neural network", variable=var, value=1, command=selection_call_back)
+chooseNN.pack()
 
-chooseCNN = Radiobutton(root, text="Use convolutional neural network", variable=var, value=2, command=hello_call_back)
-chooseCNN.grid(row=1, column=0, columnspan=2, sticky='w')
+chooseCNN = Radiobutton(root, text="Use convolutional neural network", variable=var, value=2, command=selection_call_back)
+chooseCNN.pack()
 
-chooseCNN = Radiobutton(root, text="Compare both networks", variable=var, value=3, command=hello_call_back)
-chooseCNN.grid(row=2, column=0, columnspan=2, sticky='w')
+chooseCNN = Radiobutton(root, text="Compare both networks", variable=var, value=3, command=selection_call_back)
+chooseCNN.pack()
 
-buttonTrain = Button(root, text="Train", command=train_call_back)
-buttonTrain.grid(row=3, column=0, columnspan=2, sticky='w')
+data_samples_frame = Frame(root, height=32, width=window_width); data_samples_frame.pack_propagate(0); data_samples_frame.pack()
+button = Button(data_samples_frame, text="Show data samples", command=show_data_call_back)
+button.pack(fill=BOTH, expand=1)
 
-buttonTrain = Button(root, text="Show data samples", command=show_data_call_back)
-buttonTrain.grid(row=4, column=0, columnspan=2, sticky='w')
+train_frame = Frame(root, height=32, width=window_width); train_frame.pack_propagate(0); train_frame.pack()
+button = Button(train_frame, text="Train", command=train_call_back)
+button.pack(fill=BOTH, expand=1)
 
-buttonTrain = Button(root, text="Predict", command=predict_call_back)
-buttonTrain.grid(row=5, column=0, columnspan=2, sticky='w')
+predict_frame = Frame(root, height=32, width=window_width); predict_frame.pack_propagate(0); predict_frame.pack()
+button = Button(predict_frame, text="Predict", command=predict_call_back)
+button.pack(fill=BOTH, expand=1)
 
-buttonTrain = Button(root, text="Load model", command=load_call_back)
-buttonTrain.grid(row=6, column=0, columnspan=2, sticky='w')
+load_frame = Frame(root, height=32, width=window_width); load_frame.pack_propagate(0); load_frame.pack()
+button = Button(load_frame, text="Load model", command=load_call_back)
+button.pack(fill=BOTH, expand=1)
 
-buttonTrain = Button(root, text="Save model", command=save_call_back)
-buttonTrain.grid(row=7, column=0, columnspan=2, sticky='w')
+save_frame = Frame(root, height=32, width=window_width); save_frame.pack_propagate(0); save_frame.pack()
+button = Button(save_frame, text="Save model", command=save_call_back)
+button.pack(fill=BOTH, expand=1)
+
+nn_frame = Frame(root, height=text_box_size, width=window_width); nn_frame.pack_propagate(0); nn_frame.pack(padx=5, pady=5)
+nn_info = Text(nn_frame)
+nn_info.insert(INSERT, "Simple neural network:\n")
+nn_info.insert(INSERT, "Accuracy: {}\n".format(nn_stats[0]))
+nn_info.insert(INSERT, "Value accuracy: {}\n".format(nn_stats[1]))
+nn_info.insert(INSERT, "Loss: {}\n".format(nn_stats[2]))
+nn_info.insert(INSERT, "Value loss: {}\n".format(nn_stats[3]))
+nn_info.insert(INSERT, "Time to train: {}".format(nn_stats[4]))
+nn_info.config(background='#%02x%02x%02x' % (240, 240, 240))
+nn_info.pack(fill=BOTH, expand=1)
+
+cnn_frame = Frame(root, height=text_box_size, width=window_width); cnn_frame.pack_propagate(0); cnn_frame.pack(padx=5, pady=5)
+cnn_info = Text(cnn_frame)
+cnn_info.insert(INSERT, "Convolutional neural network:\n")
+cnn_info.insert(INSERT, "Accuracy: {}\n".format(cnn_stats[0]))
+cnn_info.insert(INSERT, "Value accuracy: {}\n".format(cnn_stats[1]))
+cnn_info.insert(INSERT, "Loss: {}\n".format(cnn_stats[2]))
+cnn_info.insert(INSERT, "Value loss: {}\n".format(cnn_stats[3]))
+cnn_info.insert(INSERT, "Time to train: {}".format(cnn_stats[4]))
+cnn_info.config(background='#%02x%02x%02x' % (240, 240, 240))
+cnn_info.pack()
 
 root.mainloop()
